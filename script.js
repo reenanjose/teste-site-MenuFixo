db8f9c826fdca91aeefe9bf8b1e6564b63fa6d3d
reenanjose <interrenan@hotmail.com>
2022-05-29 20:42:06 -0300

    up

--- script.js
+++ script.js
@@ -1,5 +1,3 @@
+//let nome = prompt('Olá, qual o seu nome?')
+//let SeuNome = alert(nome  +  ' , seja bem-vinda! Aqui você irá conhecer um pouco mais do meu trabalho!')
 
-let nome = prompt('Olá, qual o seu nome?')
-let SeuNome = alert(nome  +  ' , seja bem-vinda! Aqui você irá conhecer um pouco mais do meu trabalho!')
-
-let style = window.getElementbyId('nome')
